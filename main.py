a595c074177733ff71ce602397abfdbc532f796f
Mark Ruzicka <marker11778@gmail.com>
2025-01-12 09:09:58 -0600

    added a way to generate a static website so this, will be able to be hosted for free

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ def blog(post_name):
     return flask.render_template_string(html_content)
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", 8080)
+    app.run(host="0.0.0.0", port=8080, debug=False)
